de910564b8cb09e227b791b84dfbf6f64b20f369
Mango <54734106+Developer-Mango@users.noreply.github.com>
2021-02-20 16:29:02 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace ID_2_Token
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ID;
+            Console.Write("\nEnter Users ID: ", "\n");
+            ID = Console.ReadLine();
+
+            var PlainBytes = Encoding.UTF8.GetBytes(ID);
+            string EncodedBytes = Convert.ToBase64String(PlainBytes);
+
+            Console.WriteLine("\nStart Of Token: {0}", EncodedBytes);
+            Console.ReadLine();
+        }
+    }
+}
